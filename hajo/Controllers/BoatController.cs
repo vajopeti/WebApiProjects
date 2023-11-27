ae60b5e26754fd3201f5be1a5c6c6872ac60b590
Péter Vajó <peter.vajo@stud.uni-corvinus.hu>
2023-11-27 14:54:37 +0100

    hajos projekt elso fele

---
+++ hajo/Controllers/BoatController.cs
@@ -0,0 +1,21 @@
+﻿using hajo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hajo.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public IActionResult MindegyHogyHivjak()
+        {
+            HajosContext context = new HajosContext();
+            var kérdések = from x in context.Questions select x.Question1;
+
+            return Ok(kérdések);
+        }
+    }
+}
---
+++ hajo/Controllers/TesztController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hajo.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class TesztController : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult M1()
+        {
+            string pontosIdő = DateTime.Now.ToShortTimeString();
+
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain, //"text/plain"
+                Content = pontosIdő
+            };
+        }
+    }
+}
